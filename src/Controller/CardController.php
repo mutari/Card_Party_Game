cdcfde1a86cb0941f51dd11c9cd80e037797e014
mutari <pjohansson01@yahoo.com>
2020-10-04 01:38:33 +0200

    added db

---
+++ src/Controller/CardController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Card;
+use App\Form\CardType;
+use App\Repository\CardRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/card")
+ */
+class CardController extends AbstractController
+{
+    /**
+     * @Route("/", name="card_index", methods={"GET"})
+     */
+    public function index(CardRepository $cardRepository): Response
+    {
+        return $this->render('card/index.html.twig', [
+            'cards' => $cardRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="card_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $card = new Card();
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($card);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('card_index');
+        }
+
+        return $this->render('card/new.html.twig', [
+            'card' => $card,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="card_show", methods={"GET"})
+     */
+    public function show(Card $card): Response
+    {
+        return $this->render('card/show.html.twig', [
+            'card' => $card,
+        ]);
+    }
+
+    /**
+     * @Route("/one/new", name="card_get_one_new", methods={"GET"})
+     */
+    public function getOneNew(Request $request, CardRepository $cardRep): Response
+    {   
+        
+        $amount_cards = $cardRep->createQueryBuilder('a')
+            ->select('count(a.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        return new Response($this->GenerateNewCard($cardRep, $request, $amount_cards));
+
+        return $this->redirectToRoute('card_get_one', array('id' => $random_id));
+    }
+
+    /**
+     * @Route("/one/{id}", name="card_get_one", methods={"GET"})
+     */
+    public function getOne(Request $request, Card $card)
+    {
+        return $this->render('card/oneCard.html.twig', [
+            'card' => $card
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="card_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Card $card): Response
+    {
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('card_index');
+        }
+
+        return $this->render('card/edit.html.twig', [
+            'card' => $card,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="card_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Card $card): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$card->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($card);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('card_index');
+    }
+
+
+    public function GenerateNewCard(CardRepository $cardRep, Request $request, $amount_cards) 
+    {
+        $random_id = rand(1, $amount_cards);
+
+        $res = $cardRep->find($random_id);
+
+        $CardTags = explode(",", $res->getTag());
+        $UserTags = explode(",", $request->query->get("tags"));
+
+        foreach($CardTags as $CardValue) 
+        {
+            $bool = false;
+            foreach($UserTags as $UserValue)
+            {
+                if($CardValue == $UserValue) 
+                {
+                    $bool = true;
+                    break;
+                }
+            }
+            if(!$bool)
+                return $this->GenerateNewCard($cardRep, $request, $amount_cards);
+        }
+
+        return $random_id;
+    }
+
+}
---
+++ src/Entity/Card.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CardRepository::class)
+ */
+class Card
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $Type;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Tag;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->Type;
+    }
+
+    public function setType(string $Type): self
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->Tag;
+    }
+
+    public function setTag(string $Tag): self
+    {
+        $this->Tag = $Tag;
+
+        return $this;
+    }
+}
